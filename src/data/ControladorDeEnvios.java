f229fece0f98026cfab0cbcac97aa9fb6234a4c5
nicosalgadogit <ni.salgados@duocuc.cl>
2026-08-31 04:08:33 -0400

    Semana 3: Sumativa 1

---
+++ src/data/ControladorDeEnvios.java
@@ -0,0 +1,31 @@
+package data;
+
+import model.Pedido;
+import model.Despachable;
+import model.Cancelable;
+import model.Rastreable;
+import java.util.ArrayList;
+
+public class ControladorDeEnvios implements Despachable, Cancelable, Rastreable {
+
+    private ArrayList<Pedido> historial = new ArrayList<>();
+
+    @Override
+    public void despachar(Pedido p) {
+        historial.add(p);
+        System.out.println("Pedido #" + p.getIdPedido() + " despachado con éxito.");
+    }
+
+    @Override
+    public void cancelar(Pedido p) {
+        System.out.println("Pedido #" + p.getIdPedido() + " cancelado.");
+    }
+
+    @Override
+    public void verHistorial() {
+        System.out.println("--- Historial de despachos ---");
+        for (Pedido p : historial) {
+            p.mostrarResumen();
+        }
+    }
+}
