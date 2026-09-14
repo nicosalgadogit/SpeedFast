34f044950e718fe8c3ec84f69beffd77fd3d79cb
nicosalgadogit <ni.salgados@duocuc.cl>
2026-09-13 21:20:42 -0300

    Semana 4: formativa

--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,38 +1,46 @@
 package ui;
 
-import model.Pedido;
-import model.PedidoComida;
-import model.PedidoEncomienda;
-import model.PedidoExpress;
-import data.ControladorDeEnvios;
+import model.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
 
-        ControladorDeEnvios controlador = new ControladorDeEnvios();
+        List<Pedido> pedidos1 = new ArrayList<>();
+        pedidos1.add(new PedidoComida(true, 123, "Calle los toros", 4));
+        pedidos1.add(new PedidoExpress(true, 124, "Calle los patos", 2));
 
-        Pedido pedido1 = new PedidoComida(true, 1234, "Calle los patos 123", 5);
-        pedido1.mostrarResumen();
-        pedido1.asignarRepartidor();
-        pedido1.asignarRepartidor("Juan Topo");
-        System.out.println("Tiempo estimado de entrega: " + pedido1.calcularTiempoEntrega() + " min\n----------------------------------");
 
-        Pedido pedido2 = new PedidoEncomienda(10, true, 1234, "Calle javier 123", 7.6);
-        pedido2.mostrarResumen();
-        pedido2.asignarRepartidor();
-        pedido2.asignarRepartidor("Juan pato");
-        System.out.println("Tiempo estimado de entrega: " + pedido2.calcularTiempoEntrega() + " min\n----------------------------------");
+        List<Pedido> pedidos2 = new ArrayList<>();
+        pedidos2.add(new PedidoComida(true, 153, "Calle los robles", 6));
+        pedidos2.add(new PedidoExpress(true, 154, "Calle los perros", 5));
 
-        Pedido pedido3 = new PedidoExpress(true, 136, "calle tomas 123", 15);
-        pedido3.mostrarResumen();
-        pedido3.asignarRepartidor();
-        pedido3.asignarRepartidor("Juan pez");
-        System.out.println("Tiempo estimado de entrega: " + pedido3.calcularTiempoEntrega() + " min\n----------------------------------");
+        List<Pedido> pedidos3 = new ArrayList<>();
+        pedidos3.add(new PedidoComida(true, 127, "Calle los gatos", 7));
+        pedidos3.add(new PedidoExpress(true, 126, "Calle los pajaros", 8));
 
-        controlador.despachar(pedido1);
-        controlador.despachar(pedido2);
-        controlador.cancelar(pedido3);
 
-        controlador.verHistorial();
+        Repartidor r1 = new Repartidor("Juan", pedidos1);
+        Repartidor r2 = new Repartidor("Pedro", pedidos2);
+        Repartidor r3 = new Repartidor("Jose", pedidos3);
+
+        ExecutorService executor = Executors.newFixedThreadPool(3);
+
+        executor.submit(r1);
+        executor.submit(r2);
+        executor.submit(r3);
+
+        executor.shutdown();
+        try {
+            executor.awaitTermination(1, TimeUnit.MINUTES);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+        System.out.println("Todas las entregas han finalizado.");
     }
+
 }
